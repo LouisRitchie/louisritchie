a39daae68ab8ef51c0144942f009d0477438ed35
Louis Ritchie <louiscritchie@gmail.com>
2019-02-22 00:08:13 -0800

    write router code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import 'styles/index.css'
 import 'styles/variables.css'
 import App from 'containers/app'
-import { BrowserRouter } from 'react-router-dom'
+import HireMe from 'containers/app'
+import Portfolio from 'containers/app'
+import Error from 'containers/app'
+import Timeline from 'containers/app'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 class Entry extends Component {
   render() {
     return (
       <BrowserRouter>
-        <App />
+        <Switch>
+          <Route path='/' component={App} />
+          <Route path='/hireme' component={HireMe} />
+          <Route path='/timeline' component={Timeline} />
+          <Route path='/portfolio' component={Portfolio} />
+          <Route path='/404' component={Error} />
+        </Switch>
       </BrowserRouter>
     )
   }
